aef6dee92f9e7aabe4a0eb3adaa4f8e5ff59d4c8
Krishna Chandu <kchandu@team.dgtl-factory.com>
2020-04-11 18:36:56 -0500

    First Commit

---
+++ src/main/java/ATS/software/ProjectTracker/UserRepository.java
@@ -0,0 +1,11 @@
+package ATS.software.ProjectTracker;
+
+import org.springframework.data.repository.CrudRepository;
+import ATS.software.ProjectTracker.User;;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
