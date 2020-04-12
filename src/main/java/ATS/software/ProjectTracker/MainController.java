f2be5dd9cb1b0e3d41b783151b1f1964a72b00ab
Krishna Chandu <kchandu@team.dgtl-factory.com>
2020-04-12 12:31:33 -0500

    Added a form to enter User details

--- src/main/java/ATS/software/ProjectTracker/MainController.java
+++ src/main/java/ATS/software/ProjectTracker/MainController.java
@@ -2,28 +2,36 @@ package ATS.software.ProjectTracker;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+  
 @Controller 
 @RequestMapping(path="/demo")
 public class MainController {
   @Autowired
   private UserRepository userRepository;
 
-  @PostMapping(path="/add")
-  public @ResponseBody String addNewUser (@RequestParam String name
-      , @RequestParam String email) {
-
-    User n = new User();
-    n.setName(name);
-    n.setEmail(email);
-    userRepository.save(n);
-    return "Saved";
+  
+  //Give a form to add user details. 
+  @GetMapping(path = "/addUser")
+  public String addUser(Model model)
+  {
+	  model.addAttribute("user", new User());
+	  return "user";
   }
+  
+  //After submitted button is pressed on the user form. 
+  @PostMapping(path = "/addUser")
+  public @ResponseBody String userFormSubmitted (@ModelAttribute User user) {
+	    userRepository.save(user);
+	    return "Saved";
+	  }
+
 
   @GetMapping(path="/all")
   public @ResponseBody Iterable<User> getAllUsers() {
