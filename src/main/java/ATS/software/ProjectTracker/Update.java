812f60668a6ea8a51d612c2a7f4405b06f7eb746
Krishna Chandu <kchandu@team.dgtl-factory.com>
2020-04-27 16:48:13 -0500

    Added Model Table

--- src/main/java/ATS/software/ProjectTracker/Update.java
+++ src/main/java/ATS/software/ProjectTracker/Update.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -14,7 +16,36 @@ public class Update {
 	 @GeneratedValue(strategy = GenerationType.AUTO)
 	 private Long id;
 	 
-	 @OneToOne(cascade = CascadeType.ALL)
-	 @JoinColumn(name = "project_id", referencedColumnName = "id")
+	 @ManyToOne @MapsId("userId")
+	 private User user;
+	 @ManyToOne @MapsId("projectId")
 	 private Project project;
+	 @ManyToOne @MapsId("modelId")
+	 private Model model;
+	 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Project getProject() {
+		return project;
+	}
+	public void setProject(Project project) {
+		this.project = project;
+	}
+	public Model getModel() {
+		return model;
+	}
+	public void setModel(Model model) {
+		this.model = model;
+	}
+	 
 }
